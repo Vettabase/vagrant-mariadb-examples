568f2dbf1a5bcacb8e308b5d29597d2200dcab20
Federico Razzoli <federico.razzoli@vettabase.com>
2021-01-11 02:36:14 +0000

    Add comments to Bash blocks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+# Install MariaDB from official repositories
 apt-get update -y
 sudo apt-get install -y software-properties-common dirmngr apt-transport-https
 sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
@@ -8,17 +9,21 @@ sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.ukfast.c
 apt-get update -y
 apt install -y mariadb-server
 
+# Emptty conf.d and add our my.cnf.
+# Files in this directory are included last,
+# so they can override settings in other files
 rm -Rf /etc/mysql/conf.d/*
 cp /Vagrant/files/my.cnf /etc/mysql/conf.d/
 chown -R root:root /etc/mysql/conf.d
-
 systemctl restart mariadb
 
+# Download useful tools
 mkdir -p /opt/tools
 echo 'PATH=$PATH:/opt/tools' >> /etc/profile
+# List duplicate indexes
 wget -P /opt/tools https://www.percona.com/get/pt-duplicate-key-checker
 chmod -R u+x /opt/tools
 
-# create informative views for developers
+# Create informative views for developers
 mariadb < /Vagrant/files/dev_views.sql
 
